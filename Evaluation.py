01fa40599ec0dff3f3b5dc20dd6fc99530b851eb
Stephan Dhanraj <sdhanraj@uwo.ca>
2025-03-09 13:33:04 -0400

    Tested Evaluator and created RAGAS questions and Ground Truths

--- Evaluation.py
+++ Evaluation.py
@@ -1,5 +1,9 @@
 from ragas.integrations.langchain import EvaluatorChain
-from ragas.metrics import faithfulness, context_precision, context_recall, answer_relevancy
+from ragas import EvaluationDataset
+from ragas.llms import LangchainLLMWrapper
+from ragas.metrics import LLMContextRecall, Faithfulness, FactualCorrectness
+from ragas import evaluate
+
 from LLM import LLM_Chain
 from langchain_chroma import Chroma
 from langchain_ollama import OllamaEmbeddings
@@ -10,13 +14,11 @@ from dotenv import load_dotenv
 api_key = os.environ.get("OPENAI_API_KEY")
 openai.api_key = api_key
 
+from langchain_openai import ChatOpenAI
+
 class Evaluator(): 
     def __init__(self): 
         # create evaluation chains
-        self.faithfulness_chain = EvaluatorChain(metric=faithfulness)
-        self.answer_rel_chain = EvaluatorChain(metric=answer_relevancy)
-        self.context_rel_chain = EvaluatorChain(metric=context_precision)
-        self.context_recall_chain = EvaluatorChain(metric=context_recall)
         self.eval_result = []
         self.test_questions  = [
         "Which CEO is widely recognized for democratizing AI education through platforms like Coursera?",
@@ -34,25 +36,32 @@ class Evaluator():
             "Arvind Krishna is the CEO of IBM and has transformed the company towards cloud computing and AI solutions. He focuses on delivering cutting-edge technologies to address modern business challenges.",
         ]
 
-        self.examples = [
-            {"input": q, "ground_truth": [self.test_groundtruths[i]]}
-            for i, q in enumerate(self.test_questions)
-            ]
+        self.examples = zip(self.test_questions, self.test_groundtruths)
 
         self.llm = LLM_Chain()
         self.embeddings = OllamaEmbeddings(model="all-minilm") 
         self.db = Chroma(persist_directory="./Chromadb", embedding_function=self.embeddings)
-        self.chain= self.llm.get_qa_chain(self.db)
-        self.answers = []
-        self.contexts = []
+        self.chain= self.llm.get_qa_chain()
+        self.dataset = []
+        self.evaluator_llm = LangchainLLMWrapper(ChatOpenAI(model="gpt-4o-mini"))
 
     def evaluate_chain(self): 
-        for question in self.test_questions:
-            response = self.chain.invoke({"query" : question})
-            self.answers.append(response["result"])
-            self.contexts.append([context.page_content for context in response['source_documents']])
+        for pair in self.examples:
+            #pair[0] = test_questions
+            #pair[1] = ground truths
+            response = self.chain.invoke({"input" : pair[0]})
+            self.dataset.append(
+                {
+                    "user_input": pair[0],
+                    "retrieved_contexts": [context.page_content for context in response['context']],
+                    "response": response["answer"],
+                    "reference": pair[1],
+                })
+            
+        evaluation_dataset = EvaluationDataset.from_list(self.dataset)
 
+        result = evaluate(dataset=evaluation_dataset,metrics=[LLMContextRecall(), Faithfulness(), FactualCorrectness()],llm=self.evaluator_llm)
+        print(result)
 if __name__ == "__main__":
     eval = Evaluator()
-    faithfulness = eval.evaluate_faithfulness()
-    print(faithfulness)
+    eval.evaluate_chain()
